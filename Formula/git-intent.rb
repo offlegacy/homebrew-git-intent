85afe005594af5fb18dea4ce90b1596f7867d462
kangju2000 <kangju2000@gmail.com>
2025-04-13 21:36:07 +0900

    Add git-intent formula

---
+++ Formula/git-intent.rb
@@ -0,0 +1,19 @@
+class GitIntent < Formula
+  desc "Git workflow tool for intentional commits"
+  homepage "[https://github.com/offlegacy/git-intent"](https://github.com/offlegacy/git-intent")
+  license "MIT"
+  version "0.0.0"
+
+  on_macos do
+    url "[https://github.com/offlegacy/git-intent/releases/download/v#{version}/git-intent-#{version}-darwin-amd64.tar.gz"](https://github.com/offlegacy/git-intent/releases/download/v#{version}/git-intent-#{version}-darwin-amd64.tar.gz")
+    sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  end
+
+  def install
+    bin.install "git-intent"
+  end
+
+  test do
+    system "#{bin}/git-intent", "--version"
+  end
+end
