b05b525b19eb261b4b2980523c6ae4f25039b9f4
kangju2000 <23312485+kangju2000@users.noreply.github.com>
2025-04-13 17:55:30 +0000

    Update git-intent to v0.0.3

--- Formula/git-intent.rb
+++ Formula/git-intent.rb
@@ -2,11 +2,11 @@ class GitIntent < Formula
   desc "Git workflow tool for intentional commits"
   homepage "https://github.com/offlegacy/git-intent"
   license "MIT"
-  version "0.0.2"
+  version "0.0.3"
 
   on_macos do
     url "https://github.com/offlegacy/git-intent/releases/download/v#{version}/git-intent-#{version}-darwin-amd64.tar.gz"
-    sha256 "b519858ffba83811f6e835df240f41c4d47a84af7d190e6099751993a8d3cf8b"
+    sha256 "9d180199c83702c90a8b25ac64c30a8893e60859ee2ff7f86f0ed98126e4fde5"
   end
 
   def install
